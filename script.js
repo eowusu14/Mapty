135112ca038748274bb164b54ee52a00193087c0
Hydra33 <64472767+Hydra33@users.noreply.github.com>
2023-05-03 23:06:44 +0000

    added workout formr

--- script.js
+++ script.js
@@ -11,48 +11,81 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-if (navigator.geolocation) 
-navigator.geolocation.getCurrentPosition(function(position) {
-    console.log(position);
-    const { latitude } = position.coords
-    const { longitude } = position.coords
-    // console.log(latitude, longitude);
+let map, mapEvent;
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      console.log(position);
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      // console.log(latitude, longitude);
 
-    const coords = [latitude, longitude]
+      const coords = [latitude, longitude];
 
-    const map = L.map('map').setView(coords, 18);
+      map = L.map('map').setView(coords, 18);
 
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'    
-    }).addTo(map);
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }).addTo(map);
+
+      const marker = L.marker(coords)
+        .addTo(map)
+        .bindPopup('You are here')
+        .openPopup();
+
+      L.circleMarker(coords, { radius: position.coords.accuracy }).addTo(map);
+
+      // handling clicks on map
+      map.on('click', function (mapE) {
+        mapEvent = mapE
+        form.classList.remove('hidden');
+        inputDistance.focus();
+
+        // console.log(e);
+        // const {lat, lng} = e.latlng
+        // const ptCoords = [lat, lng]
+
+        // const marker2 = L.marker(ptCoords)
+        // .addTo(map)
+        // .bindPopup(
+        //     L.popup({minWidth: 100, maxWidth: 250, autoClose: false, closeOnClick: false, className: 'running-popup'})
+        //     )
+        // .setPopupContent('Workout')
+        // .openPopup()
+      });
+    },
+
+    function () {
+      console.log('allow geolocation');
+    }
+  );
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault()
+    console.log(mapEvent);
+    const { lat, lng } = mapEvent.latlng;
+    const ptCoords = [lat, lng];
     
-    const marker = L.marker(coords)
+    const marker2 = L.marker(ptCoords)
     .addTo(map)
-    .bindPopup('You are here')
+    .bindPopup(
+        L.popup({
+            minWidth: 100,
+            maxWidth: 250,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup',
+        })
+        )
+        .setPopupContent('Workout')
     .openPopup();
     
-    L.circleMarker(coords, {radius: position.coords.accuracy})
-    .addTo(map)
-
-    map.on('click', function(e){
-        // console.log(e);
-        const {lat, lng} = e.latlng
-        const ptCoords = [lat, lng]
-
-        const marker2 = L.marker(ptCoords)
-        .addTo(map)
-        .bindPopup(
-            L.popup({minWidth: 100, maxWidth: 250, autoClose: false, closeOnclick: false, className: 'running-popup'})
-            )
-        .setPopupContent('Workout')
-        .openPopup()
-
+    inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '' 
+    inputType.addEventListener('change', function () {
+        inputElevation.closest('.form__row').classList.toggle('form__row--hidden')
+        inputCadence.closest('.form__row').classList.toggle('form__row--hidden')
     })
 
-
-}, 
-
-function(){
-    console.log('allow geolocation');}
-)
+});
